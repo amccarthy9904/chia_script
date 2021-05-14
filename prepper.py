bbab1b170f6616e8cb3b4df1cfafdb2bffb6f3db
Aaron Mcarthy <amccarthy9904@gmail.com>
2021-05-14 12:45:31 -0700

    init, prepes file system

---
+++ prepper.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+class Prepper:
+    
+    plot = None
+    farm = None
+    dirs = {}
+    
+    def __init__(self, farm, plot):
+        self.plot = plot
+        self.farm = farm
+        
+    def prep(self):
+        root = "/media/lron/plot1/p1." + self.plot
+        tst1 = root +  "/p1." + self.plot + "t1"
+        tst2 = root +  "/p1." + self.plot + "t2"
+        farm = "/media/lron/farm1/f" + self.farm + "." + self.plot
+        
+        self.dirs = {"r" : root, "t1" : tst1, "t2" : tst2, "f": farm}
+        self.clean(True)
+        
+        os.mkdir(root)
+        os.mkdir(tst1)
+        os.mkdir(tst2)
+        os.mkdir(farm)
+        return self.dirs
+        
+        
+    def clean(self, clean_farm = False):
+        shutil.rmtree(self.dirs["r"])
+        if clean_farm:
+            shutil.rmtree(self.dirs["f"])
